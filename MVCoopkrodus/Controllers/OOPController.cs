614a9a84a34f20ba8cf375afd83443bba105dda1
Opilane TTHK <opilane@tthk.ee>
2022-09-21 11:09:00 +0300

    first commit

---
+++ MVCoopkrodus/Controllers/OOPController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCoopkrodus.Controllers
+{
+    public class OOPController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVCoopkrodus/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVCoopkrodus.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
