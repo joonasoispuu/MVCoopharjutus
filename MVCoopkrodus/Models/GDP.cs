0e1c88af9b06044f13f5fd851fabdc398a8c9bc9
mole <oispuu.joonas@gmail.com>
2022-09-25 17:32:50 +0300

    Added validation

--- MVCoopkrodus/Models/GDP.cs
+++ MVCoopkrodus/Models/GDP.cs
@@ -1,12 +1,28 @@
-﻿namespace MVCoopkrodus.Models
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MVCoopkrodus.Models
 {
     public class GDP
     {
         public int Id { get; set; }
+
+        [StringLength(60, MinimumLength = 4)]
+        [Required]
         public string Country { get; set; }
         public string Region { get; set; }
+
+        [Range(1, 100000)]
+        [Required]
         public int? EstimatebyDollar { get; set; }
+
+        [Range(1, 1000000000)]
         public int? Population { get; set; }
+
+
+        [Display(Name ="Year calculated")]
+        [StringLength(4, MinimumLength = 4)]
         public string Year { get; set; }
     }
 }
