6289684b348d185e342c387dda320b402c5f68ab
mole <oispuu.joonas@gmail.com>
2022-09-27 19:03:03 +0300

    fixed problems

--- MVCoopkrodus/Models/GDP.cs
+++ MVCoopkrodus/Models/GDP.cs
@@ -11,6 +11,9 @@ namespace MVCoopkrodus.Models
         [StringLength(60, MinimumLength = 4)]
         [Required]
         public string Country { get; set; }
+
+        [StringLength(60)]
+        [Required]
         public string Region { get; set; }
 
         [Range(1, 100000)]
@@ -18,10 +21,12 @@ namespace MVCoopkrodus.Models
         public int? EstimatebyDollar { get; set; }
 
         [Range(1, 1000000000)]
+        [Required]
         public int? Population { get; set; }
 
 
         [Display(Name ="Year calculated")]
+        [Required]
         [StringLength(4, MinimumLength = 4)]
         public string Year { get; set; }
     }
